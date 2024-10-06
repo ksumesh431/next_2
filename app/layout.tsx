878cd5d48d76b2eba18b608c1ad686e3ad950db6
Sumesh Kumar Panda <sumeshpanda.k@gmail.com>
2024-10-07 05:23:29 +0530

    testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,11 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased  `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           disableTransitionOnChange
         >
           <CustomHeader />
